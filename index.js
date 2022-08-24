d48b35752ee8e9a609092a7845c3e248eb1cbf7b
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-24 20:20:29 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify({
+//       dogName: "Byron",
+//       dogBreed: "Poodle",
+//     }),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject);
+let name = "Sam"
+let email = "sam@sam.com"
+let body = document.querySelector("body");
+
+
+function submitData (userName, userEmail){
+return fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify({
+        name: userName,
+        email: userEmail
+    })
+})
+
+.then((resp)=>{
+    return resp.json()
+})
+
+.then((data)=>{
+    body.innerHTML = `<p>${data.id}</p>`
+})
+
+.catch((error)=>{
+body.innerHTML = `${error.message}`
+})
+}
+
+// submitData(name,email);
